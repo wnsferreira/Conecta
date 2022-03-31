9e1a6aa344d301df118e89a8cced74dc2d953541
viniOnRails <57204434+lanzgit@users.noreply.github.com>
2022-03-30 21:15:52 -0300

    initial front

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <router-view />
 </template>
 
@@ -19,6 +18,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
