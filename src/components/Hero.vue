3efb3a626867c67ed9e180772dde1e6f4defbd99
viniOnRails <57204434+lanzgit@users.noreply.github.com>
2022-04-04 06:57:43 -0300

    media polish

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,8 +5,8 @@
           <img class="w-50" src="../assets/Logo-Conecta.svg" alt="" width="720">
       </div>
       <div class="col-lg-7 p-3 p-lg-5 pt-lg-3" id="hero-content">
-        <h1 class="display-4 fw-bold lh-1">Chegou o Conecta</h1>
-        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur libero amet ex asperiores, vero veniam! Consequuntur cum aspernatur fugiat atque necessitatibus quidem ea! Praesentium labore officiis repellendus.</p>
+        <h1 id="title-conecta" class="display-4 fw-bold lh-1">Chegou o Conecta</h1>
+        <p class="lead" id="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur libero amet ex asperiores, vero veniam! Consequuntur cum aspernatur fugiat atque necessitatibus quidem ea! Praesentium labore officiis repellendus.</p>
         <div class="d-grid gap-2 d-md-flex justify-content-center m-5">
           <button type="button" class="btn btn-primary btn-lg px-4 me-md-2" @click="$router.push('/cadastro')">Cadastre-se</button>
           <button type="button" class="btn btn-outline-secondary btn-lg px-4" @click="$router.push('/login')">Faça Login</button>
@@ -23,6 +23,17 @@ export default {
 </script>
 
 <style>
+@media (max-width: 1199px) {
+  #desc {
+    display: none;
+  }
+}
+@media (max-width: 992px){
+  #title-conecta {
+    display: none;
+  }
+  
+}
 @media (max-width: 960px ) {
   #hero-content {
     display: none;
